f81719109bb28a972f775f6068c3f4bc7e2dced1
Golu Sen <golusen5196@gmail.com>
2026-04-02 11:59:31 +0530

    feat: add array methods examples

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ const { format } = require('date-fns')
 console.log(_.capitalize('hello from lodash'))
 console.log(format(new Date(), 'dd/MM/yyyy'))
 console.log('Axios version:', axios.VERSION)
+
+// Array methods practice
+const numbers = [1, 2, 3, 4, 5]
+const doubled = numbers.map(n => n * 2)
+console.log('Doubled:', doubled)
+
+const evens = numbers.filter(n => n % 2 === 0)
+console.log('Evens:', evens)
+
+const sum = numbers.reduce((acc, n) => acc + n, 0)
+console.log('Sum:', sum)
