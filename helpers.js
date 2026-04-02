eb9a4393bfde0df6ec50a19f4fc6755bca3d60a7
Golu Sen <golusen5196@gmail.com>
2026-04-02 12:12:22 +0530

    feat: add date helper functions

--- helpers.js
+++ helpers.js
@@ -16,3 +16,14 @@ const flattenArray = (arr) => {
 console.log(capitalizeWords('hello world from golu'))
 console.log(getUniqueItems([1, 2, 2, 3, 3, 4]))
 console.log(flattenArray([[1, 2], [3, 4], [5, 6]]))
+
+
+// Date helper functions
+const { format, addDays, differenceInDays } = require('date-fns')
+
+const formatDate = (date) => format(date, 'dd/MM/yyyy')
+const daysUntil = (date) => differenceInDays(date, new Date())
+
+const internshipEnd = addDays(new Date(), 120)
+console.log('Internship end date:', formatDate(internshipEnd))
+console.log('Days until internship ends:', daysUntil(internshipEnd))
